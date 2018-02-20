a4ead478f14de48a633f73930979725e4abb5aba
bb <biletskyboh@gmail.com>
2018-02-20 16:22:15 +0200

    added test for views

--- backend_dmm/leads/tests/test_models.py
+++ backend_dmm/leads/tests/test_models.py
@@ -4,7 +4,7 @@ from leads.models import Lead, Brief
 
 
 class LeadSetUpMixin(object):
-    """Add intial data to a TestCase."""
+    """Add intial data to a Lead TestCases."""
 
     def setUp(self):
         self.lead_data = {
@@ -13,6 +13,26 @@ class LeadSetUpMixin(object):
                     'phone': '+380930112121',
                     'message': 'I want to use dmm!'
                     }
+
+
+class BriefSetUpMixin(LeadSetUpMixin, object):
+    """Add intial data to a Brief TestCases."""
+
+    def setUp(self):
+        super(BriefSetUpMixin, self).setUp()
+        self.lead = Lead.objects.create(**self.lead_data)
+        self.brief_data = {
+            'industry': Brief.INDUSTRIES.forex.value[0],
+            'experience': Brief.EXPERIENCE.less_1.value[0],
+            'aim': Brief.AIMS.marketing.value[0],
+            'stage': Brief.STAGES.beginner.value[0],
+            'strategies': Brief.STRATEGIES.considered.value[0],
+            'audience': Brief.AUDIENCES.small_international.value[0],
+            'callcenter': Brief.CALLCENTERS.own_callcenter.value[0],
+            'marketing': Brief.MARKETING.own.value[0],
+            'payment': Brief.PAYMENTS.crypto.value[0],
+            'lead': self.lead
+        }
 
 
 class LeadModelTestCase(LeadSetUpMixin, TestCase):
@@ -33,29 +53,14 @@ class LeadModelTestCase(LeadSetUpMixin, TestCase):
         self.assertEqual(str(lead), self.lead_data['email'])
 
 
-class BriefModelTestCase(LeadSetUpMixin, TestCase):
+class BriefModelTestCase(BriefSetUpMixin, TestCase):
     """Test the Brief model."""
-
-    def setUp(self):
-        super(BriefModelTestCase, self).setUp()
-        self.brief_data = {
-            'industry': Brief.INDUSTRIES.forex.value[0],
-            'experience': Brief.EXPERIENCE.less_1.value[0],
-            'aim': Brief.AIMS.marketing.value[0],
-            'stage': Brief.STAGES.beginner.value[0],
-            'strategies': Brief.STRATEGIES.considered.value[0],
-            'audience': Brief.AUDIENCES.small_international.value[0],
-            'callcenter': Brief.CALLCENTERS.own_callcenter.value[0],
-            'marketing': Brief.MARKETING.own.value[0],
-            'payment': Brief.PAYMENTS.crypto.value[0],
-        }
-        self.lead = Lead.objects.create(**self.lead_data)
 
     def test_can_create(self):
         """Test the bucketlist model can create a bucketlist."""
 
         old_count = Brief.objects.count()
-        Brief.objects.create(lead=self.lead, **self.brief_data)
+        Brief.objects.create(**self.brief_data)
         new_count = Brief.objects.count()
         self.assertNotEqual(old_count, new_count)
 
@@ -63,5 +68,5 @@ class BriefModelTestCase(LeadSetUpMixin, TestCase):
         """Test a string representation of the model."""
 
         string_repr = f'Brief of {self.lead_data["email"]}'
-        brief = Brief.objects.create(lead=self.lead, **self.brief_data)
+        brief = Brief.objects.create(**self.brief_data)
         self.assertEqual(str(brief), string_repr)
--- backend_dmm/leads/tests/test_views.py
+++ backend_dmm/leads/tests/test_views.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 
-from leads.tests.test_models import LeadSetUpMixin
+from leads.tests.test_models import BriefSetUpMixin, LeadSetUpMixin
 
 
 class LeadCRUDTestCase(LeadSetUpMixin, APITestCase):
@@ -17,7 +17,28 @@ class LeadCRUDTestCase(LeadSetUpMixin, APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_list(self):
-        """Test the Lead creation endpoint."""
+        """Test the Lead list endpoint."""
 
         response = self.client.get(reverse('leads:list_create'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class BriefCRUDTestCase(BriefSetUpMixin, APITestCase):
+    """Test suite for the api views."""
+
+    def setUp(self):
+        super(BriefCRUDTestCase, self).setUp()
+        self.brief_data['lead'] = self.lead.email
+
+    def test_create(self):
+        """Test the Brief creation endpoint."""
+
+        response = self.client.post(reverse('leads:brief_list_create'),
+                                    self.brief_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_list(self):
+        """Test the Brief list endpoint."""
+
+        response = self.client.get(reverse('leads:brief_list_create'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
