bd81344530c23a26cf343efa560a98eddc02e852
bb <biletskyboh@gmail.com>
2018-02-20 11:55:55 +0200

    changed brief foreinkey related field

--- backend_dmm/leads/models.py
+++ backend_dmm/leads/models.py
@@ -141,7 +141,8 @@ class Brief(models.Model):
 
     date = models.DateTimeField(default=timezone.now)
     lead = models.ForeignKey(Lead, on_delete=models.CASCADE,
-                             related_name='brief')
+                             related_name='brief',
+                             to_field='email')
 
     def __str__(self):
         return f'Brief of {self.lead.email}'
