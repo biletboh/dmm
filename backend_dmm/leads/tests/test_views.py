3afdb943fa2c5f7fee5e54154d94a96ecd283f60
bb <biletskyboh@gmail.com>
2018-02-21 14:33:09 +0200

    added permissions to views

--- backend_dmm/leads/tests/test_views.py
+++ backend_dmm/leads/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 from rest_framework.test import APITestCase
 from rest_framework import status
@@ -9,9 +10,12 @@ from leads.tests.test_models import LeadSetUpMixin
 class LeadCRUDTestCase(LeadSetUpMixin, APITestCase):
     """Test suite for the api views."""
 
+    def setUp(self):
+        super(LeadCRUDTestCase, self).setUp()
+        User.objects.create_user('test', 'test@dmm.com', 'testpass10')
+
     def test_create(self):
         """Test the Lead creation endpoint."""
-
         response = self.client.post(reverse('leads:list_create'),
                                     self.lead_data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -19,5 +23,6 @@ class LeadCRUDTestCase(LeadSetUpMixin, APITestCase):
     def test_list(self):
         """Test the Lead creation endpoint."""
 
+        self.client.login(username='test', password='testpass10')
         response = self.client.get(reverse('leads:list_create'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
--- backend_dmm/leads/views.py
+++ backend_dmm/leads/views.py
@@ -1,11 +1,17 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
+from rest_condition import Or
+
 from .serializers import LeadSerializer, BriefSerializer
 from .models import Brief, Lead
+from .permissions import IsPostRequest, IsOptionsRequest
 
 
 class LeadListCreateView(generics.ListCreateAPIView):
     """Create a lead and display a list of leads."""
 
+    permission_classes = [Or(IsAuthenticated, IsPostRequest)]
     queryset = Lead.objects.all()
     serializer_class = LeadSerializer
 
@@ -13,5 +19,6 @@ class LeadListCreateView(generics.ListCreateAPIView):
 class BriefListCreateView(generics.ListCreateAPIView):
     """Create a brief and display a list of briefs."""
 
+    permission_classes = [Or(IsAuthenticated, IsPostRequest, IsOptionsRequest)]
     queryset = Brief.objects.all()
     serializer_class = BriefSerializer
