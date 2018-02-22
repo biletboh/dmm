3e262c6c76a9dac31c1cb2892070ab3ca271cb98
bb <biletskyboh@gmail.com>
2018-02-22 15:29:02 +0200

    added split settings for django app

--- backend_dmm/backend_dmm/settings/base.py
+++ backend_dmm/backend_dmm/settings/base.py
@@ -13,17 +13,14 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = os.path.dirname(os.path.dirname
+                           (os.path.dirname(os.path.abspath(__file__))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '=5y)!9als=99_wl=^l3b&uw66oiho7o)=p&nnqpxicbooli!n8'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -58,11 +55,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = (
-    'localhost:8080',
-    'test.dmm.solutions',
-)
-
 ROOT_URLCONF = 'backend_dmm.urls'
 
 TEMPLATES = [
@@ -82,17 +74,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'backend_dmm.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
@@ -121,12 +102,9 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication'
     )
 }
-
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
---
+++ backend_dmm/backend_dmm/settings/test.py
@@ -0,0 +1,17 @@
+from .base import *
+
+DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+CORS_ORIGIN_WHITELIST = (
+    os.environ.get('API_ORIGIN'),
+)
