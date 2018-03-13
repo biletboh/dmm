21d29c19d18ee20cac328c04889dab8f19cd9092
khyu1987 <khyu1987@gmail.com>
2018-03-13 13:43:54 +0200

    changed brief options animation

--- frontend/src/components/BriefOptions.vue
+++ frontend/src/components/BriefOptions.vue
@@ -1,23 +1,28 @@
 <template>
   <div v-if="briefOptions">
-    <transition name="slide-fade" mode="out-in">
     <div class="row">
-      <div class="col-sm-12 sm-margin-b-2">
-        <h2 class="text-center"> {{ titles[position] }} </h2>
-      </div>
-    </div>
-    </transition>
-  <div  v-if="briefOptions.length == 1">
-    <img src="../assets/img/all/Preloader_2.gif" class="img-responsive center-block" alt="loading">
-  </div>
-    <div class="row options-wrapper">
-      <div v-for="(choice, index) in briefOptions[position]['choices']" :key="index" class="col-sm-6 sm-margin-b-2 option wow fadeInLeft" data-wow-duration=".3" :data-wow-delay="countDelay(index+1)">
-        <div class="service" data-height="height" @click="nextQuestion(), saveBriefData(choice.value)" :id="choice.label">
-          <div class="service-info">
-            <h3>{{ choice.display_name }} </h3>
-          </div>
+              <!-- <transition name="slide-fade" mode="out-in"> -->
+        <div class="col-sm-12 sm-margin-b-2 wow fadeInLeft animated" :key="titles[position]" data-wow-duration="1.3s">
+          <h2 class="text-center"> {{ titles[position] }} </h2>
         </div>
-      </div>
+      <!-- </transition> -->
+    </div>
+    <div  v-if="briefOptions.length == 1">
+      <img src="../assets/img/all/Preloader_2.gif" class="img-responsive center-block" alt="loading">
+    </div>
+    <div class="row options-wrapper">
+      <!-- <transition-group appear name="fade" mode="out-in" tag="div"> -->
+        <div v-for="(choice, index) in briefOptions[position]['choices']" :key="index" class="col-sm-6 sm-margin-b-2 option" >
+          <!-- <transition appear name="fade" mode="out-in" > -->
+          <div data-height="height" @click="nextQuestion(), saveBriefData(choice.value)" :id="choice.label" :key="randInt()"
+            class="service wow fadeInLeft animated" data-wow-duration="1.3s" :data-wow-delay="countDelay(index+1)" >
+            <div class="service-info" :key="choice">
+              <h3>{{ choice.display_name }} </h3>
+            </div>
+          </div>
+        <!-- </transition> -->
+        </div>
+      <!-- </transition-group> -->
     </div>
   </div>
 </template>
@@ -69,6 +74,9 @@ export default {
     countDelay (delay) {
       return '.' + delay++ + 's'
     },
+    randInt () {
+      return Math.floor(Math.random() * 100)
+    },
     nextQuestion: function () {
       return this.$store.dispatch('incrementBriefCount')
     },
@@ -84,10 +92,25 @@ export default {
 }
 </script>
 <style scoped>
-  .option{
-    padding: 1px;
-  }
-  .options-wrapper{
-    min-height: 440px;
-  }
+.option{
+  padding: 1px;
+}
+.options-wrapper{
+  min-height: 440px;
+}
+
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .3s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active до версии 2.1.8 */ {
+  opacity: 0;
+}
+
 </style>
